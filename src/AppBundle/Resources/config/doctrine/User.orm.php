8d17efd238338b4e08a3b0ed1fe55a109333a02a
Xiaobai525574 <893530635@qq.com>
2018-06-06 23:26:02 +0800

    Initial commit

---
+++ src/AppBundle/Resources/config/doctrine/User.orm.php
@@ -0,0 +1,20 @@
+<?php
+
+use Doctrine\ORM\Mapping\ClassMetadataInfo;
+
+$metadata->setInheritanceType(ClassMetadataInfo::INHERITANCE_TYPE_NONE);
+$metadata->customRepositoryClassName = 'AppBundle\Repository\UserRepository';
+$metadata->setChangeTrackingPolicy(ClassMetadataInfo::CHANGETRACKING_DEFERRED_IMPLICIT);
+$metadata->mapField(array(
+   'fieldName' => 'id',
+   'type' => 'integer',
+   'id' => true,
+   'columnName' => 'id',
+  ));
+$metadata->mapField(array(
+   'columnName' => 'name',
+   'fieldName' => 'name',
+   'type' => 'string',
+   'length' => '10',
+  ));
+$metadata->setIdGeneratorType(ClassMetadataInfo::GENERATOR_TYPE_AUTO);
